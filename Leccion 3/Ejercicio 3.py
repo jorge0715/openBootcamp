b3237fd7563314731ec3f81179318abea6e9ff8d
jorge0715 <jorge.ero@hotmail.com>
2022-10-14 19:00:16 -0500

    Leccion3

---
+++ Leccion 3/Ejercicio 3.py
@@ -0,0 +1,13 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Peso = float(input("Ingrese su peso en KG: "))
+
+Ingrese su peso en KG: 70
+>>> Estatura = float(input("Ingrese su estatura en M: "))
+
+Ingrese su estatura en M: 1.70
+>>> IMC = Peso/pow(Estatura,2)
+
+>>> print(f'Su indice de masa corporal es: {IMC}')
+Su indice de masa corporal es: 24.221453287197235
+>>> 
